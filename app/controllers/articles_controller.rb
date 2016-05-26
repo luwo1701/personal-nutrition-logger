dd47a4ecbdb6d6aae8a822f68a76a4b182d53b1e
Luke Worley <luke.worley@colorado.edu>
2016-05-26 14:31:29 -0400

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+   def index 
+      @articles = Article.all
+   end
+   def show
+      @article = Article.find(params[:id])
+   end
+   def new
+      @article = Article.new
+   end
+   def create
+      @article=Article.new(article_params)#Article class Articild in models\article.rb
+      @article.save
+
+      redirect_to @article
+      #render plain: params[:article].inspect
+   end
+   private
+      def article_params
+         params.require(:article).permit(:title,:text)
+      end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+@apiKey="FrmKAzEVQQLWTvp17K7LaGnWRWuw02DhapJhbbK0"
+  def index
+  @apiKey="FrmKAzEVQQLWTvp17K7LaGnWRWuw02DhapJhbbK0" 
+  @num_foods = 10
+  url = 'http://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key='+@apiKey
+  response_resource = RestClient::Resource.new(url) #from what i understand it creates a RESTCLIENT object we can manipulate using the classes methods
+  response=response_resource.get
+  response_JSON = JSON.parse(response) #creates a ruby hash table out of the JSON object
+  10.times do |i|
+    @response = response_JSON ['list']['item'][i]["name"]
+  end 
+  end
+end
