d3357ebd8d6bfb26097de4044fd6dd7edf14a688
Luke Worley <luke.worley@colorado.edu>
2016-06-13 19:42:44 -0600

    got rid of the search view and controller and am attempting to make the whole add food process isolated to the foods resource

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,22 +1,47 @@
 class FoodsController < ApplicationController
-  def index
-	@groups=FoodGroup.all  
+  def new
+  @food_group = FoodGroup.all
+  @q=params[:q]
+  @group = params[:groupChosen]  
+  @apiKey="FrmKAzEVQQLWTvp17K7LaGnWRWuw02DhapJhbbK0" 
+  @num_foods = 10
+  foodsUrl ="http://api.nal.usda.gov/ndb/search/?format=json&q="+@q+"&fg=Baked%20Products&sort=n&max=25&offset=0&api_key="+@apiKey
+	
+  @nutrientHash={}  
+  
+   response_resource = RestClient::Resource.new(foodsUrl) #from what i understand it creates a RESTCLIENT object we can manipulate using the classes methods 
+   #404 code means resource not found.
+   response_resource.get{|response,request,result,&block|
+	@code = response.code
+ }
+   if @code!=404
+   	response = RestClient::Resource.new(foodsUrl)
+   	response = response.get
+   	@response_JSON = JSON.parse(response) #JSON.parse(@response) #creates a ruby hash table out of the JSON object 
+   		3.times do |i|
+      	ndbno=@response_JSON['list']['item'][i]['ndbno']
+      	nutrientUrl="http://api.nal.usda.gov/ndb/reports/?ndbno="+ndbno+"&type=b&format=json&api_key="+@apiKey
+      	nutrients_JSON=JSON.parse(RestClient::Resource.new(nutrientUrl).get)
+				name = @response_JSON['list']['item'][i]['name']
+				@nutrientHash[i] = {"name"=>name,"calories" => nutrients_JSON['report']['food']['nutrients'][1]['value'],"protein" => nutrients_JSON['report']['food']['nutrients'][2]['value'],"fat" =>  nutrients_JSON['report']['food']['nutrients'][3]['value'] }	
+    	end
+    else
+    @nutrientHash[0] = {"name" => "not found","calories"=> "not found","protein"=> "not found","fat"=> "not found"} 
+    
+    end
   end
+
   def search
- 	@apiKey="FrmKAzEVQQLWTvp17K7LaGnWRWuw02DhapJhbbK0"
-  	@num_foods = 10
-  	url = 'http://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key='+@apiKey
-  	response_resource = RestClient::Resource.new(url) #from what i understand it creates a RESTCLIENT object we can manipulate using the classes methods
- 	 response=response_resource.get
- 	 response_JSON = JSON.parse(response) #creates a ruby hash table out of the JSON object
- 	 3.times do |i|
-   	 @response = response_JSON ['list']['item'][i]["name"]
-	  
- 	 end
   end
+	def index
+	end
+	
+	def create
+		@food = Food.new(food_params)
+	end
   private
-      def article_params
-         params.require(:groups).permit(:menuName,:searchName)
+      def food_params
+         params.require(:food).permit(:name,:calories)
       end
 
 end
